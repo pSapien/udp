8890f5022c00fa6865441c016e6d57d52c293a57
Ranjan Shrestha <ranjan@sharingapples.com>
2020-09-15 09:30:06 +0545

    UDPSocket working draft

--- src/UdpSocket.ts
+++ src/UdpSocket.ts
@@ -1,10 +1,14 @@
 import { createSocket, Socket } from 'dgram';
 import { AddressInfo } from 'net';
 import { EndPoint, UdpStream } from './UdpStream';
-import { BufferSerializer, Serializer, Serializable, Oracle } from '@bhoos/serialization';
+import { BufferSerializer, Serializable, Oracle } from '@bhoos/serialization';
 
 const GENERAL = 0;
 const STREAM = 1;
+
+function getEndPointId(endpoint: EndPoint) {
+  return `${endpoint.address}:${endpoint.port}`;
+}
 
 export class UdpSocket<S = UdpStream> {
   private socket: Socket;
@@ -13,23 +17,59 @@ export class UdpSocket<S = UdpStream> {
 
   private handlers = new Map<number, (msg: Serializable) => void>();
   private streamHandlers = new Map<number, (msg: Serializable, userData: S, stream: UdpStream) => void>();
-  private connectHandlers = new Map<number, (msg: Serializable, stream: UdpStream) => S>();
+  private connectHandlers = new Map<number, (msg: Serializable, stream: UdpStream) => Promise<S>>();
+  private closeHandler: (userData: S, stream: UdpStream) => void;
+  private stream: UdpStream;
+  private streams: Map<string, { stream: UdpStream, userData: S}>;
+  private closing: boolean = false;
 
-  private streams = new Map<string, { stream: UdpStream, userData: S}>();
-
-  constructor(oracle: Oracle, version: number, port?: number) {
+  constructor(oracle: Oracle, version: number) {
     this.version = version;
     this.oracle = oracle;
     this.socket = createSocket('udp4');
-    if (port) {
-      this.socket.bind(port);
-    }
-
     this.socket.on('message', this.handleMessage);
   }
 
+  listen(port: number) {
+    this.socket.bind(port);
+    this.streams = new Map<string, { stream: UdpStream, userData: S}>();
+  }
+
+  connect(endpoint: EndPoint, version: number, message: Serializable): UdpStream {
+    const stream = new UdpStream(this, endpoint, version, () => {
+      this.stream = null;
+      this.closing = true;
+      this.handleClose(null, stream);
+    });
+    this.stream = stream;
+    this.stream.send(message);
+    return this.stream;
+  }
+
+  private handleClose(userData: S, stream: UdpStream) {
+    if (this.closeHandler) this.closeHandler(userData, stream);
+    if (this.closing) {
+      if (!this.stream && (!this.streams || this.streams.size === 0)) this.end();
+    }
+  }
+
   close() {
+    if (this.closing) return;
+    this.closing = true;
+
+    // close will end if there aren't any streams associated with the socket
+    if (!this.stream && (!this.streams || this.streams.size === 0)) this.end();
+
+    // Attempt to close any direct streams
+    if (this.stream) this.stream.close();
+
+    // Initiate close on all the connected streams
+    if (this.streams) this.streams.forEach(({ stream }) => stream.close());
+  }
+
+  end() {
     this.socket.off('message', this.handleMessage);
+    this.socket.close();
   }
 
   on<T extends Serializable>(clazz: new (...args:any[]) => T, handler: (msg: T) => void) {
@@ -37,55 +77,95 @@ export class UdpSocket<S = UdpStream> {
     this.handlers.set(id, handler);
   }
 
-  onConnect<T extends Serializable>(clazz: new (...args: any[]) => T, handler: (msg: T, stream: UdpStream) => S) {
+  onClose(handler: (userData: S, stream: UdpStream) => void) {
+    if (this.closeHandler) {
+      throw new Error('Close handler is already defined');
+    }
+    this.closeHandler = handler;
+  }
+
+  onConnect<T extends Serializable>(clazz: new (...args: any[]) => T, handler: (msg: T, stream: UdpStream) => Promise<S>) {
     const id = this.oracle.id(clazz);
     this.connectHandlers.set(id, handler);
   }
 
-  onStream<T extends Serializable>(clazz: new (...args:any[]) => T, handler: (msg: T, stream: S) => void) {
+  onStream<T extends Serializable>(clazz: new (...args:any[]) => T, handler: (msg: T, user: S) => void) {
     const id = this.oracle.id(clazz);
     this.streamHandlers.set(id, handler);
   }
 
-  connect(endpoint: EndPoint, version: number, message: Serializable): UdpStream {
-    const stream = new UdpStream(this, endpoint, version);
-    stream.send(message);
-    return stream;
+  raiseError(err: Error) {
+    console.log(err);
   }
 
   private handleMessage = (data: Uint8Array, rinfo: AddressInfo) => {
     const type = data[0];
+    console.log(`Rx: ${rinfo.address}:${rinfo.port}, ${type}, ${data.length}`, data);
 
-    const buffer = Buffer.from(data, 1, data.byteLength - 1);
-
+    const buffer = Buffer.from(data);
     if (type === GENERAL) {
-      const serializer = new BufferSerializer(this.version, buffer);
+      const serializer = new BufferSerializer(this.version, buffer, 1);
       const obj = this.oracle.serialize(serializer, null);
       const id = this.oracle.identify(obj);
       const handler = this.handlers.get(id);
       handler(obj);
     } else if (type === STREAM) {
-      const remoteId = `${rinfo.address}:${rinfo.port}`;
-      const endpoint = this.streams.get(remoteId);
-      if (!endpoint) {
-        const stream = new UdpStream(this, rinfo, 0);
-        const serializer = new BufferSerializer(stream.version, buffer);
-        stream.receive(serializer, (msg) => {
-          const id = this.oracle.identify(msg);
-          const connectHandler = this.connectHandlers.get(id);
-          const userData = connectHandler(msg, stream);
-          if(userData) {
-            this.streams.set(remoteId, { stream, userData });
+      if (this.stream) {
+        const serializer = new BufferSerializer(this.stream.version, buffer, 1);
+        this.stream.receive(serializer, (item: Serializable) => {
+          const id = this.oracle.identify(item);
+          const streamHandler = this.streamHandlers.get(id);
+          if (!streamHandler) {
+            this.raiseError(new Error(`No stream handler found for ${id}`));
+          } else {
+            streamHandler(item, null, this.stream);
           }
         });
       } else {
-        const { stream, userData } = endpoint;
-        const serializer = new BufferSerializer(stream.version, buffer);
-        stream.receive(serializer, (item: Serializable) => {
-          const id = this.oracle.identify(item);
-          const streamHandler = this.streamHandlers.get(id);
-          streamHandler(item, userData, stream);
-        });
+        const remoteId = getEndPointId(rinfo);
+        const endpoint = this.streams.get(remoteId);
+        if (!endpoint) {
+          const stream = new UdpStream(this, rinfo, 0, () => {
+            // Inform the socket, in case the stream has been registered
+            const ep = this.streams.get(remoteId);
+            // It is possible for the endpoint be a different stream, so make sure
+            if (ep.stream === stream) {
+              this.streams.delete(remoteId);
+              this.handleClose(ep.userData, ep.stream);
+            }
+          });
+          const serializer = new BufferSerializer(stream.version, buffer, 1);
+          stream.receive(serializer, async (msg) => {
+            const id = this.oracle.identify(msg);
+            const connectHandler = this.connectHandlers.get(id);
+            if (!connectHandler) {
+              stream.close();
+              this.raiseError(new Error(`No connection handler found ${remoteId}, ${id}`));
+            } else {
+              const userData = await connectHandler(msg, stream);
+              // Since the connect handler is async, it's possible that another request might
+              // have been received within that time
+              if(userData && !this.streams.has(remoteId)) {
+                this.streams.set(remoteId, { stream, userData });
+              } else {
+                // Get rid of the stream (without closing), just need to release the resources
+                stream.end();
+              }
+            }
+          });
+        } else {
+          const { stream, userData } = endpoint;
+          const serializer = new BufferSerializer(stream.version, buffer, 1);
+          stream.receive(serializer, (item: Serializable) => {
+            const id = this.oracle.identify(item);
+            const streamHandler = this.streamHandlers.get(id);
+            if (!streamHandler) {
+              this.raiseError(new Error(`No stream handler found for ${id}`));
+            } else {
+              streamHandler(item, userData, stream);
+            }
+          });
+        }
       }
     }
   }
@@ -94,13 +174,15 @@ export class UdpSocket<S = UdpStream> {
     const serializer = new BufferSerializer(stream.version, 1000);
     serializer.uint8(STREAM);
     stream.serialize(serializer);
-    this.socket.send(serializer.getBuffer(), stream.remote.port, stream.remote.address);
+    const buffer = serializer.getBuffer();
+    this.socket.send(buffer, 0, serializer.length, stream.remote.port, stream.remote.address);
+    console.log(`Sending data to ${stream.remote.address}:${stream.remote.port}, ${serializer.length} bytes`);
   }
 
   send(to: EndPoint, msg: Serializable) {
     const serializer = new BufferSerializer(this.version, 1000);
     serializer.uint8(GENERAL);
     this.oracle.serialize(serializer, msg);
-    this.socket.send(serializer.getBuffer(), to.port, to.address);
+    this.socket.send(serializer.getBuffer(), 0, serializer.length, to.port, to.address);
   }
 }
--- src/UdpStream.ts
+++ src/UdpStream.ts
@@ -29,14 +29,29 @@ export class UdpStream implements Stream {
   private sendHandle: ReturnType<typeof setImmediate> = null;
   private retryHandle: ReturnType<typeof setTimeout> = null;
   private retryInterval = MIN_RETRY_INTERVAL; // Perform a retry in 500 ms, failing which perform a retry in 1000 and 1500 and so on
+  private attempts: number = 0;
+  private maxAttempts: number = 10;
+  private closing: boolean = false;
+  private onClose: () => void;
 
-  constructor(socket: UdpSocket<any>, remote: EndPoint, version: number) {
+  constructor(socket: UdpSocket<any>, remote: EndPoint, version: number, onClose: () => void) {
     this.socket = socket;
     this.version = version;
     this.remote = remote;
+    this.onClose = onClose;
   }
 
   close() {
+    if (this.closing) return;
+    this.closing = true;
+    this.maxAttempts = 5;
+
+    // Initiate a send for closing
+    this.send();
+  }
+
+  end() {
+    this.closing = true;
     if (this.retryHandle) {
       clearTimeout(this.retryHandle);
       this.retryHandle = null;
@@ -47,10 +62,19 @@ export class UdpStream implements Stream {
       clearImmediate(this.sendHandle);
       this.sendHandle = null;
     }
+
+    // Trigger a close event, making sure it is called once and only once
+    if (this.onClose) {
+      const k = this.onClose;
+      this.onClose = null;
+      k();
+    }
   }
 
   receive(serializer: Serializer, handler: (item: Serializable) => void): void {
     const remoteAck = serializer.uint16(0);
+    console.log('Remote ack', remoteAck);
+
     // Remove all items that are acked
     while (this.items.length) {
       const k = this.items[0];
@@ -58,6 +82,14 @@ export class UdpStream implements Stream {
 
       // Remove the items that have been acked
       this.items.shift();
+    }
+
+    // Clean up the attempts count, since we received something
+    this.attempts = 0;
+
+    if (remoteAck === 0xffff) {
+      this.end();
+      return;
     }
 
     // Cleanup the retry interval as soon as something is received, since the ack
@@ -71,6 +103,12 @@ export class UdpStream implements Stream {
     do {
       const seq = serializer.uint16(0);
       if (seq === 0) break;
+      if (seq === 0xffff) {
+        this.remoteSeq = seq;
+        this.close();
+        this.maxAttempts = 1;
+        break;
+      }
 
       if (seq > this.remoteSeq) {
         // Something we haven't seen before, initiate an ack seq
@@ -85,22 +123,32 @@ export class UdpStream implements Stream {
 
   serialize(serializer: Serializer) {
     serializer.uint16(this.remoteSeq);
+    let remaining = this.items.length;
     for (let i = 0; i < this.items.length; i += 1) {
       const k = this.items[i];
       // Mark the serializer position for reverting back in case of error
-      serializer.mark();
+      const revert = serializer.mark();
       try {
         serializer.uint16(k.seq);
         this.socket.oracle.serialize(serializer, k.item);
+        remaining -= 1;
       } catch (err) {
-        serializer.revert();
-        serializer.uint16(0);
+        revert();
+        break;
       }
+    }
+
+    // Attach the close seq id at the end
+    if (this.closing && !remaining) {
+      serializer.uint16(0xffff);
+    } else {
+      serializer.uint16(0);
     }
   }
 
   send(item?: Serializable) {
-    if (item) {
+    // Allow additional items only if we are not already closing
+    if (item && !this.closing) {
       this.localSeq += 1;
       this.items.push({ seq: this.localSeq, item });
     }
@@ -108,17 +156,28 @@ export class UdpStream implements Stream {
     // If a send is already initiated just ignore
     if (this.sendHandle) return;
 
+    // In case of high attempts, assume a closed connection
+    if (this.attempts >= this.maxAttempts) {
+      this.end();
+      return;
+    }
+
     this.sendHandle = setImmediate(() => {
+      this.attempts += 1;
+      console.log('Send to stream attempt', this.attempts, this.maxAttempts);
       this.socket.sendStream(this);
 
       // Clear the handle, once the send is complete
       this.sendHandle = null;
 
       // Setup a retry after few seconds (not for ack msg)
-      if (this.items.length) {
+      if (this.items.length || this.closing) {
         if (this.retryHandle) clearTimeout(this.retryHandle);
         this.retryHandle = setTimeout(() => this.send(), this.retryInterval);
         if (this.retryInterval < MAX_RETRY_INTERVAL) this.retryInterval += 500;
+      } else {
+        // Clear the attempts count, after sending the ack only packet
+        this.attempts = 0;
       }
     });
   }
